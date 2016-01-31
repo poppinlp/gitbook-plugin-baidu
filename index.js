f1434ffd292fafb24a9ec031773de37b71505b5c
PoppinL <poppinlp@gmail.com>
2016-01-31 12:26:33 +0800

    version 0.1.0

--- index.js
+++ index.js
@@ -6,16 +6,10 @@ module.exports = {
         ],
         html: {
             "body:end": function() {
-                var config = this.options.pluginsConfig.baidu || {};
-
-                if (!config.token) {
-                    throw "Need to provide 'token' for Baidu Analytics plugin";
-                }
-
                 return "<script>var _hmt = _hmt || [];"
                     + "(function() {"
                     + "var hm = document.createElement('script');"
-                    + "hm.src = '//hm.baidu.com/hm.js?" + config.token + "';"
+                    + "hm.src = '//hm.baidu.com/hm.js?" + this.options.pluginsConfig.baidu.token + "';"
                     + "var s = document.getElementsByTagName('script')[0];"
                     + "s.parentNode.insertBefore(hm, s);"
                     + "})();</script>";
